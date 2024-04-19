955d4661feedd416e9d6f779def4721e0ea1348a
andrsand <angorko@list.ru>
2024-04-20 00:38:15 +0300

    example lines

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ function resize() {
     canvas.height = 500;
 }
 
-function clearCanvas() {
-    canvas.width = canvas.width;
-}
+// context.beginPath(); // начало новой элементарной геометрической фигуры
+// context.moveTo(0, 0); // ставим перо в верхний левый угол
+// context.lineTo(300, 400); // декларация намерения нарисовать прямую линию с определенными координатами
+// context.stroke(); // реализация рисования прямой линии
+
+// context.beginPath();
+// context.moveTo(300, 400);
+// context.lineTo(500, 0);
+// context.stroke(); 
 
 context.beginPath();
 context.moveTo(0, 0);
-context.lineTo(500, 500);
-context.stroke();
+context.lineTo(300, 400);
+context.lineTo(500, 0);
+context.strokeStyle = 'red';
+context.lineWidth = 5;
+context.stroke(); 
